24bd781a8fc46ae5cb62239536363a111998cfed
isaacShin-dev <isaacshin92@gmail.com>
2023-06-28 11:24:44 +0900

    main page // login modal layout

--- blogpjt/src/App.vue
+++ blogpjt/src/App.vue
@@ -32,11 +32,12 @@
             <v-btn icon>
                 <v-icon>mdi-magnify</v-icon>
             </v-btn>
-            <v-btn icon>
+            <v-btn icon @click="toGit">
                 <v-icon>mdi-github</v-icon>
             </v-btn>
-
-
+            <v-btn @click="loginModal">
+                <v-icon>mdi-login</v-icon>
+            </v-btn>
         </v-app-bar>
 
         <v-main>
@@ -46,6 +47,22 @@
                 </transition>
             </router-view>
         </v-main>
+        <div class="modal-wrapper" v-show="showModal">
+            <div class="modal-area">
+                <v-card height="350" elevation="10" rounded>
+                    <v-card-title>로그인</v-card-title>
+                    <v-card-text class="mt-4">
+                        <v-text-field label="아이디"></v-text-field>
+                        <v-text-field label="비밀번호" type="password"></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn>로그인</v-btn>
+                        <v-btn disabled="true">회원가입</v-btn>
+                    </v-card-actions>
+                </v-card>
+
+            </div>
+        </div>
 
     </v-app>
 </template>
@@ -58,17 +75,49 @@ export default {
     setup () {
         const theme = useTheme()
         const drawer = ref(false)
+        const showModal = ref(false)
 
         return {
             drawer,
+            showModal,
             theme,
             toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark',
             toWork: () => router.push({name: 'works'}),
             toLog: () => router.push({name: 'log'}),
-            toContact: () => router.push({name: 'contact'})
+            toContact: () => router.push({name: 'contact'}),
+            toGit: () => window.open('https://github.com/isaacShin-dev'),
+            loginModal:() => showModal.value = !showModal.value
 
         }
     },
 }
 </script>
+
+<style scoped>
+    .modal-wrapper {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 999;
+        width: 100%;
+        height: 100%;
+        overflow-x: hidden;
+        overflow-y: auto;
+        background-color: rgba(0,0,0,0.5);
+        //display: none;
+    }
+    .modal-area{
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 400px;
+        height: 400px;
+        background-color: #fff;
+        border-radius: 10px;
+        padding: 20px;
+        box-sizing: border-box;
+        box-shadow: 0 0 20px rgba(0,0,0,0.5);
+    }
+</style>
 
